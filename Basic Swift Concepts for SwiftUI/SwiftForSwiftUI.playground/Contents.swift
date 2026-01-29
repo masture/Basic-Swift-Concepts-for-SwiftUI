b6b95176b9c68be186fe71c371de663ccae4df55
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2026-01-29 14:54:36 +0530

    #2 - constants and variables

--- Basic Swift Concepts for SwiftUI/SwiftForSwiftUI.playground/Contents.swift
+++ Basic Swift Concepts for SwiftUI/SwiftForSwiftUI.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let sessionName = "Swift for SwiftUI"
+print(sessionName)
+
+var numberOfParticipants = 50
+print(numberOfParticipants)
+
+// Say 10 more joined
+numberOfParticipants += 10 // this is same as numberOfParticipants = numberOfParticipants + 10
+print(numberOfParticipants)
+
+
