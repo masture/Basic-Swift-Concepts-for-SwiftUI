8c19cd1a83cfe18a2a8ef219d276f11e4621d8c6
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2026-01-30 21:41:11 +0530

    #16 - Used the TDCardView from the ContentView

--- Basic Swift Concepts for SwiftUI/ContentView.swift
+++ Basic Swift Concepts for SwiftUI/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let aTerm = TermDefinition(term: "SwiftUI", definition: "A modern way to declare user interfaces for any Apple platform. Create beautiful, dynamic apps faster than ever before.", termColor: "Blue")
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            TDCardView(someTerm: aTerm)
+            
+            
         }
         .padding()
     }
